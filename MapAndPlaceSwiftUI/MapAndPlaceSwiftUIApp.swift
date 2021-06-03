c9332bc5b7b7f41baa58a2f889f46e5dc58a9098
Alexey Barinov <begoodbebad2012@gmail.com>
2021-06-03 17:23:51 +0300

    Initial Commit

---
+++ MapAndPlaceSwiftUI/MapAndPlaceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAndPlaceSwiftUIApp.swift
+//  MapAndPlaceSwiftUI
+//
+//  Created by Mac on 03.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapAndPlaceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
